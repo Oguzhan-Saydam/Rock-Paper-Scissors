27faa9426e9fe3b48297a5bb4e56c2d5b17a561f
Oguzhan-Saydam <oguzhansaydam22@gmail.com>
2022-11-09 16:32:20 +0100

    One round of rock-paper-scissors complete

---
+++ javascript.js
@@ -0,0 +1,48 @@
+let treshhold1 = 34;
+let treshhold2 = 67;
+let randomNumber = Math.random() * 100;
+
+function getComputerChoice() {
+    if (randomNumber < treshhold1) {
+        return "rock";
+    }
+    else if (randomNumber < treshhold2) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+}
+
+//console.log(getComputerChoice());
+
+let playerSelection = prompt("Select");
+playerSelection = playerSelection.toLowerCase();
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You won! Rock beats scissors"
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You won! Paper bears rock"
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You won! Scissors beats paper"
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lost! Paper beats rock"
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You lost! Rock beats scissors"
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You lost! Scissors beats paper"
+    } else if (playerSelection == "rock" && computerSelection == "rock") {
+        return "Draw!"
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        return "Draw!"
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        return "Draw!"
+    } else {
+        return "Please enter a valid value!"
+    }
+}
+
+
+console.log(playRound(playerSelection, computerSelection));
